201472782518af0e39dd71b5d1d89c0ad55d9da7
Shiloh <emily.chiu000@gmail.com>
2024-10-30 15:21:01 -0400

    confirmed that model can be loaded and used in another python file.

--- test.py
+++ test.py
@@ -1,29 +1,31 @@
-import tensorflow as tf
-import os
+""" this file is used to make sure that the model can be called from the Jupyter Notebook in a regular python file. """
+
+# import needed dependencies
 import cv2
 from matplotlib import pyplot as plt
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
-from keras.callbacks import EarlyStopping
-from tensorflow.keras.metrics import Precision, Recall, BinaryAccuracy
-import cv2
+import tensorflow as tf
 from tensorflow.keras.models import load_model
+import os
+import numpy as np
+from matplotlib import pyplot as plt
 
+
+# load in model
 new_model = load_model(os.path.join('models','melanomamodel.h5'))
-new_model.compile('adam', loss = tf.losses.BinaryCrossentropy(), metrics = ['accuracy'])
 
-img = cv2.imread('benigntest3.jpg')
-# plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+# load in the image
+img = cv2.imread('data/test/benigntest3.jpg')
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 # plt.show()
 
 resize = tf.image.resize(img, (256,256))
 np.expand_dims(resize, 0)
-yhat = model.predict(np.expand_dims(resize/255,0))
 
+# using the new model
 yhat_new = new_model.predict(np.expand_dims(resize/255, 0))
-if yhat > 0.75:
+if yhat_new > 0.75:
     print('Predicted class is malignant.')
-elif yhat < 0.50:
+elif yhat_new < 0.50:
     print('Predicted class is benign.')
 else:
     print('Predicted class is undetermined.')
