8513fbfc19c61c93e32e2ac827b536c5209af2af
Shiloh <emily.chiu000@gmail.com>
2024-11-28 21:40:34 -0500

    fixing problems

--- app/app.py
+++ app/app.py
@@ -1,20 +1,35 @@
 from flask import Flask, render_template, request, Response, redirect, url_for
 import os
 import time
-import picamera
+#from picamera import PiCamera # this doesn't work
 import tensorflow as tf
 from PIL import Image
 import numpy as np
 from datetime import datetime
 
+import cv2
+from matplotlib import pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
+from keras.callbacks import EarlyStopping
+from tensorflow.keras.metrics import Precision, Recall, BinaryAccuracy
+import cv2
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+
 app = Flask(__name__)
 
-#camera setup
-camera = picamera.PiCamera()
-camera.resolution = (640, 480)
+# #camera setup
+# camera = picamera.PiCamera()
+# camera.resolution = (640, 480)
+
 
 #loading the model
-model = tf.keras.models.load_model('/path/to/model.h5')
+model = load_model(os.path.join('models','melanomamodel.h5'))
+model.compile('adam', loss = tf.losses.BinaryCrossentropy(), metrics = ['accuracy'])
 
 #directory to save the images and the logs
 os.makedirs('static/images', exist_ok=True)
@@ -22,9 +37,12 @@ log_file = 'static/screening_log.txt'
 
 #stream camera footage on the web page
 def gen_frames():
-	while True:
+	# while True:
+	for _ in range(1000):
 		#capture frame by frame
-		camera.capture('static/images/live.jpg', 'jpeg')
+
+		os.system("libcamera-still -o static/images/live.jpg")
+		# time.sleep(0.083)
 		with open('static/images/live.jpg', 'rb') as live_img:
 			yield (b'--frame\r\n'
 				b'Content-Type: image/jpeg\r\n\r\n' + live_img.read() + b'\r\n')
@@ -46,10 +64,11 @@ def index():
 def capture_image():
 	image_path = 'static/images/pic.jpg'
 	#capture image with camera
-	camera.start_preview()
-	time.sleep(2) # allow time for the camera to adjust
-	camera.capture(image_path)
-	camera.stop_preview()
+	os.system("libcamera-still -o " + image_path)
+	# camera.start_preview()
+	# time.sleep(2) # allow time for the camera to adjust
+	# camera.capture(image_path)
+	# camera.stop_preview()
 
 	return redirect(url_for('process_image'))
 
@@ -72,7 +91,8 @@ def process_image():
 
 @app.route('/video_feed')
 def video_feed():
-	return Response(gen_frames(), minetype='multiport/x-mixed-replace; boundary=frame')
+	return Response(gen_frames(),
+				    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
